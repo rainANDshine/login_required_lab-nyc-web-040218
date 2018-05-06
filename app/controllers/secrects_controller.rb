344caf1b8638e1b77cb0a5e5da212d65723d9130
Shun Yao <ysmiracle@hotmail.com>
2018-05-06 23:29:58 +0000

    automatically backed up by learn

--- app/controllers/secrects_controller.rb
+++ app/controllers/secrects_controller.rb
@@ -1,4 +1,11 @@
 class SecrectsController < ApplicationController
+  before_action :require_login
+  
   def show
+  end
+
+  private
+  def require_login
+    return login_path unless session.include? :name
   end
 end
