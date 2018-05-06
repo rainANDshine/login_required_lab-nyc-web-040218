5378bf80b259d1ba1c81af0e3d91dde78b833906
Shun Yao <ysmiracle@hotmail.com>
2018-05-06 23:19:01 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:name] && !params[:name].empty?
+      session[:name] = params[:name]
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
